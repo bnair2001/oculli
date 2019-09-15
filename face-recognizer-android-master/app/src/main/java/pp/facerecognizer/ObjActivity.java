8741883d4377dc2d690495d2a1e36b0f4ebadcd6
mooosu <mooosu@hotmail.com>
2019-09-16 00:06:13 +0930

    merge httppost;add network_security_config

--- face-recognizer-android-master/app/src/main/java/pp/facerecognizer/ObjActivity.java
+++ face-recognizer-android-master/app/src/main/java/pp/facerecognizer/ObjActivity.java
@@ -1,5 +1,6 @@
 package pp.facerecognizer;
 
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,12 +10,19 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.util.Log;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.UUID;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ObjActivity extends AppCompatActivity {
+public class ObjActivity extends AppCompatActivity implements UploadFileAsyncTaskCallback<UploadFileAsyncTask.Result>{
     private static final int CONTENT_REQUEST=1337;
     private File output=null;
     @Override
@@ -47,12 +55,53 @@ public class ObjActivity extends AppCompatActivity {
                                     Intent data) {
         if (requestCode == CONTENT_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Intent i=new Intent(Intent.ACTION_VIEW);
+                Log.e("onActivityResult", output.getPath());
+                try {
+                    FileInputStream fileInputStream = new FileInputStream(output);
+                    new UploadFileAsyncTask(this).execute(fileInputStream);
+                } catch (Exception e) {
+                    Log.e("onActivityResult", output.getPath(),e);
+                }
 
-                i.setDataAndType(Uri.fromFile(output), "image/jpeg");
-                startActivity(i);
-                finish();
+                //Intent i=new Intent(Intent.ACTION_VIEW);
+
+                // i.setDataAndType(Uri.fromFile(output), "image/jpeg");
+
+                //startActivity(i);
+
+                //finish();
             }
+        }
+    }
+
+    @Override
+    public void onResponse(UploadFileAsyncTask.Result result) {
+        if( !result.isErrorResponse && !result.hasException()) {
+            String path = Environment.getExternalStorageDirectory() + "/" + UUID.randomUUID().toString() +  ".mp3";
+            File file = new File(path);
+            try {
+                FileOutputStream fOut = new FileOutputStream(file);
+                byte [] data = result.body.bytes();
+                fOut.write(data);
+                fOut.close();
+                result.filename = path;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    @Override
+    public void finishDownloading(List<UploadFileAsyncTask.Result> results) {
+        for(UploadFileAsyncTask.Result result: results) {
+            System.out.println(result.filename);
+            Uri uri = Uri.fromFile(new File(result.filename));
+            Log.e("onActivityResult", result.filename);
+
+            MediaPlayer player = MediaPlayer.create(this, uri);
+            player.setLooping(false);
+            player.setVolume(100, 100);
+            player.start();
         }
     }
 }
