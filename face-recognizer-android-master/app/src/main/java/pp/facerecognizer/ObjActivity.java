ed77a5cd913e06040002d33c0f24778b6ff33c33
mooosu <mooosu@hotmail.com>
2019-09-15 22:22:20 +0930

    add upload file task

--- face-recognizer-android-master/app/src/main/java/pp/facerecognizer/ObjActivity.java
+++ face-recognizer-android-master/app/src/main/java/pp/facerecognizer/ObjActivity.java
@@ -2,13 +2,16 @@ package pp.facerecognizer;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ObjActivity extends AppCompatActivity {
 
@@ -19,4 +22,23 @@ public class ObjActivity extends AppCompatActivity {
 
     }
 
+    public static final MediaType MEDIA_TYPE_MARKDOWN
+            = MediaType.get("text/x-markdown; charset=utf-8");
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void run() throws Exception {
+        File file = new File("README.md");
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/markdown/raw")
+                .post(RequestBody.create(file, MEDIA_TYPE_MARKDOWN))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            System.out.println(response.body().string());
+        }
+    }
 }
